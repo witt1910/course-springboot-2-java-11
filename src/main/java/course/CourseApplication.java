01fc77bbeb0e919338a18a92e6c61dc8b17fecce
Wagner Witt <wittwizard@gmail.com>
2020-06-14 16:23:44 -0300

    Project created

---
+++ src/main/java/course/CourseApplication.java
@@ -0,0 +1,14 @@
+package course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		// Primeiro comando para a aplicação SpringBoot rodar
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
